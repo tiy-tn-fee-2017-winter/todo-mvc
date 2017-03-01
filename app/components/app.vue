2ea4ed1affe928a0c06e19f60797a013bec26444
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-01 13:54:24 -0600

    Create presentational component

--- app/components/app.vue
+++ app/components/app.vue
@@ -1,13 +1,21 @@
 <template lang="html">
   <div class="">
     <ul>
-      <li><button v-on:click="increment">{{ number }}</button></li>
+      <counter-button
+        v-bind:number="number"
+        v-on:change="increment"></counter-button>
     </ul>
   </div>
 </template>
 
 <script>
+import CounterButton from './counter-button.vue';
+
 export default {
+  // What custom elements/components can we use in our template
+  components: {
+    CounterButton,
+  },
   data() {
     return {
       number: 0,
---
+++ app/components/counter-button.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <li>
+    <button v-on:click="$emit('change')">{{ number }}</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['number'],
+
+  methods: {
+
+  },
+};
+</script>
